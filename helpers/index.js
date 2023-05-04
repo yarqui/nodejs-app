f2b532239059f3060879dc6bad4328d5b64a2897
Yaroslav Pelykh <y.pelykh@gmail.com>
2023-05-04 20:57:46 +0300

    edit listContacts

--- helpers/index.js
+++ helpers/index.js
@@ -2,5 +2,12 @@ const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseError = require("./handleMongooseError");
 const editImage = require("./editImage");
+const sendEmail = require("./sendEmail");
 
-module.exports = { HttpError, ctrlWrapper, handleMongooseError, editImage };
+module.exports = {
+  HttpError,
+  ctrlWrapper,
+  handleMongooseError,
+  editImage,
+  sendEmail,
+};
--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
+
 const { SENDGRID_API_KEY, ZL_PASSWORD, SG_EMAIL_SENDER, ZL_EMAIL_SENDER } =
   process.env;
 
@@ -20,25 +21,27 @@ const sendSgEmail = async (data) => {
   return true;
 };
 
-// const sendNodemailerEmail = (data) => {
-//   const config = {
-//     host: "mail.adm.tools",
-//     port: 465,
-//     secure: true,
-//     auth: {
-//       user: ZL_EMAIL_SENDER,
-//       pass: ZL_PASSWORD,
-//     },
-//   };
-//   // creates transporter with configuration
-//   const transporter = nodemailer.createTransport(config);
-//   const message = { ...data, from: ZL_EMAIL_SENDER };
-//   transporter.sendMail(message);
+const sendNodemailerEmail = (data) => {
+  const config = {
+    host: "mail.adm.tools",
+    port: 465,
+    secure: true,
+    auth: {
+      user: ZL_EMAIL_SENDER,
+      pass: ZL_PASSWORD,
+    },
+  };
+  // creates transporter with configuration
+  const transporter = nodemailer.createTransport(config);
+  const message = { ...data, from: ZL_EMAIL_SENDER };
+  transporter.sendMail(message);
 
-//   return true;
-// };
+  return true;
+};
 
 sendSgEmail(testDataToSend);
-sendNodemailerEmail(testDataToSend);
+// sendNodemailerEmail(testDataToSend);
 
-module.exports = { sendSgEmail };
+const sendEmail = { sendSgEmail, sendNodemailerEmail };
+
+module.exports = sendEmail;
